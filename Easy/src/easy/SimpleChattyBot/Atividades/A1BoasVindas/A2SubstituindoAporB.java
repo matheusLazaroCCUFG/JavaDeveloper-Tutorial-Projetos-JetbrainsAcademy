8dc6645495ecc681df8531e607aa907e518808a2
Matheus <matheuscrf09@discente.ufg.br>
2021-09-30 18:11:01 -0300

    commit

---
+++ Easy/src/easy/SimpleChattyBot/Atividades/A1BoasVindas/A2SubstituindoAporB.java
@@ -0,0 +1,13 @@
+package easy.SimpleChattyBot.Atividades.A1BoasVindas;
+
+import java.util.Scanner;
+
+public class A2SubstituindoAporB {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+
+        str = str.replace('a', 'b');
+        System.out.println(str);
+    }
+}
---
+++ Easy/src/easy/SimpleChattyBot/Atividades/A1BoasVindas/A5VerificarValor.java
@@ -0,0 +1,16 @@
+package easy.SimpleChattyBot.Atividades.A1BoasVindas;
+
+import java.util.Scanner;
+
+public class A5VerificarValor {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int val = scan.nextInt();
+
+        if(val < 10 && val > 0){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
+}
